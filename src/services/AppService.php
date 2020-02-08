014a95804f5748e8dff36c01f705f8c14bcf86be
HeatherHyer <heather.hyer@familysearch.org>
2020-02-08 10:49:07 -0700

    reset db info

--- src/services/AppService.php
+++ src/services/AppService.php
@@ -1,14 +1,14 @@
 <?php
-abstract class AppService 
+abstract class AppService
 {
     public $dbcon = "";
     public $id = "";
     public $body = NULL;
     public $default_db_conf = array(
-        "host" => "127.0.0.1",
-        "user" => "Heather",
-        "pass" => "qEzZ6)q#NAbu@a)",
-        "db" => "lms3660"
+        "host" => "",
+        "user" => "",
+        "pass" => "",
+        "db" => ""
     );
     public $db_conf = NULL;
 
@@ -25,24 +25,24 @@ abstract class AppService
         else
             $this->db_conf = $conf;
 
-        $dbhost=$this->db_conf["host"]; 
-        $dbuser=$this->db_conf["user"]; 
-        $dbpass=$this->db_conf["pass"]; 
-        $dbname=$this->db_conf["db"]; 
+        $dbhost=$this->db_conf["host"];
+        $dbuser=$this->db_conf["user"];
+        $dbpass=$this->db_conf["pass"];
+        $dbname=$this->db_conf["db"];
         $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        $this->dbcon = $dbh;
     }
-    
+
      function sanitize($field) {
         $field = filter_var(trim($field), FILTER_SANITIZE_STRING);
         return $field;
-    } 
+    }
 
     function required($field, $name) {
         if(!empty($field)){
             return FALSE;
-        } 
+        }
         else {
             return "Error: ". $name . " is required";
         }
@@ -51,7 +51,7 @@ abstract class AppService
     function validateEmail($field) {
         if(filter_var($field, FILTER_VALIDATE_EMAIL)){
             return FALSE;
-        } 
+        }
         else {
             return "Error: must enter valid email";
         }
@@ -60,12 +60,12 @@ abstract class AppService
     function validatePhone($field) {
         if (filter_var($field, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[0-9]{10}$/")))){
             return FALSE;
-        } 
+        }
         else {
             return "Error: phone number must be 10 digits";
         }
     }
-    
+
     protected function parseRequest($request)
     {
         $this->body = json_decode($request->getBody());
@@ -76,6 +76,6 @@ abstract class AppService
     abstract public function delete();
     abstract public function read();
     abstract public function list();
-    
+
 }
 ?>
